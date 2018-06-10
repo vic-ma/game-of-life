60cf41449c78a48957cb4176ac02f8989e6e1db3
Victor Ma <vic.ma@mail.utoronto.ca>
2018-06-10 17:51:31 -0400

    Create GameBoard class with empty methods

---
+++ golg.py
@@ -0,0 +1,12 @@
+class GameBoard:
+    def __init__(self, x: int, y: int) -> None:
+        pass
+    
+    def birth(self, x: int, y:int) -> None:
+        pass
+
+    def kill(self, x: int, y:int) -> None:
+        pass
+
+    def tick(self) -> None:
+        pass
