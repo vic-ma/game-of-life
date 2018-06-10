2f88c34b47c9ae6bb0553b4fdefc942ce4cc2cf4
Victor Ma <vic.ma@mail.utoronto.ca>
2018-06-10 18:03:06 -0400

    Create Cell class with empty methods

--- golg.py
+++ golg.py
@@ -10,3 +10,17 @@ class GameBoard:
 
     def tick(self) -> None:
         pass
+
+class Cell:
+    def __init__(self) -> None:
+        pass
+
+    def birth(self) -> None:
+        pass
+
+    def kill(self) -> None:
+        pass
+
+    def tick(self) -> None:
+        pass
+
